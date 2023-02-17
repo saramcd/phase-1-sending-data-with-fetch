f4b23edd074d33e88c376c4627c1dc9e9637578e
Sara McDonnell <saramcdonnell32@gmail.com>
2023-02-16 19:48:39 -0500

    lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+const submitData = (userName, userEmail) => {
+  const formData = {
+    name: userName,
+    email: userEmail,
+  };
+  const configurationObject = {
+    method: "POST",
+    headers: { "Content-Type": "application/json", Accept: "application/json" },
+    body: JSON.stringify(formData),
+  };
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      const newHeader = document.createElement("h1");
+      newHeader.textContent = object.id;
+      document.body.append(newHeader);
+    })
+    .catch(function (error) {
+      const newHeader = document.createElement("h1");
+      newHeader.textContent = error.message;
+      document.body.append(newHeader);
+    });
+};
